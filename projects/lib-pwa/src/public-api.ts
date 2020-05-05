5d8fa66ca91bf6e52eca5f4200edeaaa1894abef
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-05 15:50:09 +0200

    Ordered the files so as to have the library module file first in the list.

--- projects/lib-pwa/src/public-api.ts
+++ projects/lib-pwa/src/public-api.ts
@@ -2,8 +2,8 @@
  * Public API Surface of lib-pwa
  */
 
-export * from './lib/material.module';
 export * from './lib/lib-pwa.module';
+export * from './lib/material.module';
 export * from './lib/pwa-prompt.directive';
 export * from './lib/pwa-prompt-icon.component';
 export * from './lib/pwa-prompt.component';
