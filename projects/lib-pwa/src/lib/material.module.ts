4ad807a3f2c2d7cab9dd25f122330973a1c3b979
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-14 14:38:58 +0200

    Added the missing material button module to the app-pwa project.

--- projects/lib-pwa/src/lib/material.module.ts
+++ projects/lib-pwa/src/lib/material.module.ts
@@ -4,12 +4,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatIconModule,
     MatTooltipModule,
+    MatButtonModule,
     MatBottomSheetModule
   ]
 })
